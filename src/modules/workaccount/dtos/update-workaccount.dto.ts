50b046be66bffed2cfc9327acb42fa785ee67290
v0sska <speedatom80k@gmail.com>
2024-10-23 16:00:38 +0300

    added a new field in update-workaccount.dto

--- src/modules/workaccount/dtos/update-workaccount.dto.ts
+++ src/modules/workaccount/dtos/update-workaccount.dto.ts
@@ -1,4 +1,11 @@
 import { CreateWorkAccountDto } from './create-workaccount.dto';
 import { PartialType } from '@nestjs/swagger';
+import { IsBoolean } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateWorkAccountDto extends PartialType(CreateWorkAccountDto) {}
+
+export class UpdateWorkAccountDto extends PartialType(CreateWorkAccountDto) {
+	@ApiPropertyOptional()
+    @IsBoolean()
+	isBlocked?: boolean;
+}
