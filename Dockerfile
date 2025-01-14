41933afe0c331380133f6e9883eb322b51ecc3ea
v0sska <speedatom80k@gmail.com>
2025-01-14 15:31:11 +0200

    created a Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:22-alpine
+
+RUN apk add --no-cache openssl
+
+RUN mkdir /api
+RUN chown node:node /api
+USER node
+WORKDIR /api
+
+COPY --chown=node:node nest-cli.json package*.json tsconfig.build.json tsconfig.json ./
+RUN npm ci
+COPY --chown=node:node src ./src
+COPY --chown=node:node prisma ./prisma
+COPY --chown=node:node .env ./
+
+RUN npm run prisma:generate
+RUN npm run build
+
+EXPOSE 3000
+
+CMD [ "npm", "run", "start:prod" ]
--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,27 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import config from './common/configs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: config.server.frontendUrl,
     credentials: true,
   });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Report example')
     .setDescription('The report API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
